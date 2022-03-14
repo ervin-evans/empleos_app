2535c46792884b9c5f9358c6542e4d3777d87ac1
Ervin Triano <ervintdeveloper@gmail.com>
2022-03-14 15:18:07 -0600

    Se agregaron los CDN para Bootstrap y Jquery, se revomovieron los archivos locales

--- src/main/java/com/evans/EmpleosApplication.java
+++ src/main/java/com/evans/EmpleosApplication.java
@@ -2,6 +2,7 @@ package com.evans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class EmpleosApplication {
